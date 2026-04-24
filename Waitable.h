215404b14d8f756bd24785e6f1396ed08f879f31
Donato Curvino <dhcgamertime09@gmail.com>
2026-04-24 00:41:50 -0500

    implemented waiting on a predicate

--- Waitable.h
+++ Waitable.h
@@ -18,22 +18,35 @@ public:
         std::condition_variable_any
     >;
     using lockable_type = Lockable<T, Mtx>;
+    using locked_type   = Locked  <T, Mtx>;
 
     using lockable_type::Lockable;
 
     Waitable(const Waitable& other) : lockable_type{other} {}
     Waitable& operator=(const Waitable& other) { lockable_type::operator=(other); return *this; }
     Waitable(Waitable&& other) : lockable_type{std::move(other)} {}
-    // using lockable_type::operator=(const lockable_type&);
     Waitable& operator=(Waitable&& other) { lockable_type::operator=(std::move(other)); return *this; }
 
-    Locked<T, Mtx> wait()
+    locked_type wait()
     {
         std::unique_lock lock(lockable_type::mtx);
         cv.wait(lock);
         lock.release();
         return lock(std::adopt_lock);
     }
+
+    template <typename Pred>
+        requires std::same_as<bool, std::invoke_result_t<Pred>>
+    locked_type wait(Pred&& pred)
+    {
+        std::unique_lock lock(lockable_type::mtx);
+        cv.wait(lock, std::forward<Pred>(pred));  // TODO: what if pred needs accessed to lockable_type::value?
+        lock.release();
+        return lock(std::adopt_lock);
+    }
+
+    void notify_one() { cv.notify_one(); }
+    void notify_all() { cv.notify_all(); }
 
 private:
     condition_type cv{};
